ed72b7d15bfac979217924d1774cf15597d9923c
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-09-11 12:27:24 -0400

    Add files via upload

---
+++ AreaClass/ShapeLibrary/Shape.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary
+{
+    public class Shape
+    {
+        private string _length;
+        private string _width;
+        private string _heigth;
+        private string _pi;
+
+        public Shape()
+        {
+            Length = "";
+            Width = "";
+            Height = "";
+            Pi = "";
+        }
+        public Shape(string length, string width, string height, string pi)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+            Pi = pi;
+        }
+        public string Length
+        {
+            get
+            {
+                return _length;
+            }
+            set
+            {
+                _length = value;
+            }
+        }
+        public string Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+        public string Height
+        {
+            get
+            {
+                return _heigth;
+            }
+            set
+            {
+                _heigth = value;
+            }
+        }
+        public string Pi
+        {
+            get
+            {
+                return _pi;
+            }
+            set
+            {
+                _pi = value;
+            }
+        }
+        //public static double Shape(double pi, double radius)
+        //{
+        //    double circle = pi * radius * radius;
+        //    return circle;
+        //}
+        //public static double Shape(double length, double width)
+        //{
+        //    double rectangle = length * width;
+        //    return rectangle;
+        //}
+        //public static double Shape(double pi, double radius, double height)
+        //{
+        //    double cylinder = 2 * pi * (radius * radius) + height * (2 * pi * radius);
+        //    return cylinder;
+        //}
+    }
+}
---
+++ AreaClass/ShapeLibrary/StandardMessages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeLibrary
+{
+    public class StandardMessages
+    {
+        public static string Length()
+        {
+            return "What is the length? ";
+        }
+        public static string Width()
+        {
+            return "What is the width? ";
+        }
+        public static string Height()
+        {
+            return "What is the height? ";
+        }
+        public static string Radius()
+        {
+            return "What is the radius? ";
+        }
+        public static string CircleArea(double cir)
+        {
+            return $"The circle area is {cir}";
+        }
+        public static string RectangleArea(double rec)
+        {
+            return $"The rectangle area is {rec}";
+        }
+        public static string CylinderArea(double cyl)
+        {
+            return $"The cylinder area is {cyl}";
+        }
+    }
+}
