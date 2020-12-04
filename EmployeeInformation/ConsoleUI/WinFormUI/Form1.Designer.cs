e193897413321be2e7bdce52c14127a0f33b064c
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-12-04 13:55:45 -0500

    Add files via upload

---
+++ EmployeeInformation/ConsoleUI/WinFormUI/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WinFormUI
+{
+    partial class EmployeeClass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.Label();
+            this.ageLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.phoneTextBox = new System.Windows.Forms.TextBox();
+            this.ageTextBox = new System.Windows.Forms.TextBox();
+            this.employeeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(140, 34);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(75, 13);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "nameQuestion";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.AutoSize = true;
+            this.phoneLabel.Location = new System.Drawing.Point(140, 74);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(116, 13);
+            this.phoneLabel.TabIndex = 1;
+            this.phoneLabel.Text = "phoneNumberQuestion";
+            // 
+            // ageLabel
+            // 
+            this.ageLabel.AutoSize = true;
+            this.ageLabel.Location = new System.Drawing.Point(140, 112);
+            this.ageLabel.Name = "ageLabel";
+            this.ageLabel.Size = new System.Drawing.Size(67, 13);
+            this.ageLabel.TabIndex = 2;
+            this.ageLabel.Text = "ageQuestion";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(531, 27);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // phoneTextBox
+            // 
+            this.phoneTextBox.Location = new System.Drawing.Point(531, 67);
+            this.phoneTextBox.Name = "phoneTextBox";
+            this.phoneTextBox.Size = new System.Drawing.Size(100, 20);
+            this.phoneTextBox.TabIndex = 4;
+            // 
+            // ageTextBox
+            // 
+            this.ageTextBox.Location = new System.Drawing.Point(531, 105);
+            this.ageTextBox.Name = "ageTextBox";
+            this.ageTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ageTextBox.TabIndex = 5;
+            // 
+            // employeeButton
+            // 
+            this.employeeButton.Location = new System.Drawing.Point(343, 276);
+            this.employeeButton.Name = "employeeButton";
+            this.employeeButton.Size = new System.Drawing.Size(143, 23);
+            this.employeeButton.TabIndex = 6;
+            this.employeeButton.Text = "Submit Employee";
+            this.employeeButton.UseVisualStyleBackColor = true;
+            this.employeeButton.Click += new System.EventHandler(this.employeeButton_Click);
+            // 
+            // EmployeeClass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.employeeButton);
+            this.Controls.Add(this.ageTextBox);
+            this.Controls.Add(this.phoneTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.ageLabel);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "EmployeeClass";
+            this.Text = "EmployeeClass";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.Label ageLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox phoneTextBox;
+        private System.Windows.Forms.TextBox ageTextBox;
+        private System.Windows.Forms.Button employeeButton;
+    }
+}
+
---
+++ EmployeeInformation/ConsoleUI/WinFormUI/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EmployeeLibrary;
+
+namespace WinFormUI
+{
+    public partial class EmployeeClass : Form
+    {
+        public EmployeeClass()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nameLabel.Text = StandardMessages.AskForName();
+            phoneLabel.Text = StandardMessages.AskForPhoneNumber();
+            ageLabel.Text = StandardMessages.AskForAge();
+        }
+
+        private void employeeButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"{nameTextBox.Text}\n {phoneTextBox.Text}\n {ageTextBox.Text}");
+        }
+    }
+}
