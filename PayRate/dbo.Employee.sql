e2da93eeb22b5823c4887de2c23aae32eb5dd511
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-12-04 14:25:12 -0500

    Add files via upload

---
+++ PayRate/dbo.Employee.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [employee_ID]     INT        IDENTITY (1, 1) NOT NULL,
+    [name]            NCHAR (50) NOT NULL,
+    [position]        NCHAR (50) NOT NULL,
+    [hourly_pay_rate] NCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([employee_ID] ASC)
+);
+
