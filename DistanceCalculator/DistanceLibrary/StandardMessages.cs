0b563941da71660a65eafc8c842f52d5a70e3d0a
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-12-04 13:26:11 -0500

    Add files via upload

---
+++ DistanceCalculator/DistanceLibrary/StandardMessages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceLibrary
+{
+    public static class StandardMessages
+    {
+        public static string ObjectSpeed()
+        {
+            return "What was the speed the object was going?";
+        }
+
+        public static string ObjectTime()
+        {
+            return "How many hours it took for the object to get to the location?";
+        }
+
+        public static string ObjectDistance(double length)
+        {
+            return $"The object traveled {length} miles.";
+        }
+    }
+}
