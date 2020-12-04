4615762a5b198f18d0229bc36e92ac5355fab203
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-12-04 13:43:47 -0500

    Add files via upload

---
+++ ClassLibrary/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class Calculator
+    {
+        public static double CalculateRetail(double wholesale, double markup)
+        {
+            double retail = wholesale * (markup / 100) + wholesale;
+            return retail;
+        }
+    }
+}
---
+++ ClassLibrary/StandardMessages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public static class StandardMessages
+    {
+        public static string AskForMarkup()
+        {
+            return "What is the markup percentage? ";
+        }
+
+        public static string AskForCost()
+        {
+            return "What is the cost? ";
+        }
+
+        public static string DisplayRetail(double retail)
+        {
+            return $"The retail price is ${retail}";
+        }
+    }
+}
