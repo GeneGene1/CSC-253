c4e8ad29df18d50b9e1fe38efa631191e9e39be9
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-12-04 14:14:07 -0500

    Add files via upload

---
+++ MultiformEmployee/Employee/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Employee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personnelDataSet.Employee' table. You can move, or remove it, as needed.
+            this.employeeTableAdapter.Fill(this.personnelDataSet.Employee);
+
+        }
+
+        private void employeeBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.employeeBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.personnelDataSet);
+
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ShowDetailButton_Click(object sender, EventArgs e)
+        {
+            DetailForm detail = new DetailForm();
+
+            detail.ShowDialog();
+
+            this.employeeTableAdapter.Fill(this.personnelDataSet.Employee);
+        }
+    }
+}
