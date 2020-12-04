800f58cc348462cf31a656adf491bd63e116a2c3
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-12-04 14:29:20 -0500

    Delete dbo.Employee.sql

--- HourlyPaySorter/Employee/Employee/PayRate/dbo.Employee.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Employee] (
-    [employee_ID]     INT        IDENTITY (1, 1) NOT NULL,
-    [name]            NCHAR (50) NOT NULL,
-    [position]        NCHAR (50) NOT NULL,
-    [hourly_pay_rate] NCHAR (50) NOT NULL,
-    PRIMARY KEY CLUSTERED ([employee_ID] ASC)
-);
-
