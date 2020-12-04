221c9d6ab411f00e526afa769a7f91fd9110a1cd
GeneGene1 <47635902+GeneGene1@users.noreply.github.com>
2020-12-04 13:05:27 -0500

    Add files via upload

---
+++ DistanceCalculator/ConsoleUI/Program.cs
@@ -0,0 +1,49 @@
+﻿/**
+ * 9.26.20
+ * CSC 253
+ * Eugene Davis-Paulino
+ * Creating a program that calculate distance
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double time;
+            double speed;
+            double length;
+
+            Console.WriteLine(DistanceLibrary.StandardMessages.ObjectSpeed());
+            input = Console.ReadLine();
+
+            Double.TryParse(input, out speed);
+
+            Console.WriteLine(DistanceLibrary.StandardMessages.ObjectTime());
+            input = Console.ReadLine();
+
+            Double.TryParse(input, out time);
+
+            length = CallDistance(speed, time);
+
+            Console.WriteLine(DistanceLibrary.StandardMessages.ObjectDistance(length));
+            input = Console.ReadLine();
+
+            Double.TryParse(input, out length);
+        }
+
+        public static double CallDistance(double speed, double time)
+        {
+            double distance = speed * time;
+            return distance;
+        }
+    }
+}
